2ce73bb079989b5284b2b516c5ba5aa4fba51f46
Matt <mmszek3000@gmail.com>
2018-02-05 14:30:41 -0500

    Optimize index.js for a more isolated environment

--- index.js
+++ index.js
@@ -4,23 +4,21 @@
  * License: Apache 2.0
  */
 
-/* global $ */ /* global projectId */
-
 // Don't use this as the editor; use it only for interacting with it
 // Browserify your code and add it to the end of the main file
 
-function enableShare(postURL, callback) {
-    if (!postURL) return;
-    $('#share-menu').style({
+function enableShare(postURL, button, callback) {
+    if (!postURL || !button) return;
+    button.style({
         cursor: 'default'
     });
-    $('#share-menu').click((data, textStatus, jqXHR) => {
+    button.click((data, textStatus, jqXHR) => {
         if (callback) callback(data, textStatus, jqXHR);
     });
 }
 
-function setPlayer(url) {
-    $('#player-view').click(function() {
+function setPlayer(url, projectId, button) {
+    button.click(function() {
         window.location = `${url}#${projectId}`;
     });
 }
